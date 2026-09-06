e1ac59087e146d7d0c3315db38177517e217d1fa
Saurabh Singh Bisht <saurabhbisht950@gmail.com>
2026-09-06 10:28:38 +0530

    Time: 0 ms (100%), Space: 9.4 MB (30.56%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>t(m , vector<int>(n));
+
+t[0][0] = 1;
+for(int i =0 ; i<m; i++){
+    t[i][0]= 1 ;
+
+}
+
+for(int j = 0 ; j<n ; j++){
+    t[0][j]= 1 ; 
+
+}
+
+for(int i =1 ; i<m ; i++){
+    for(int j=1 ; j<n ; j++){
+t[i][j]= t[i-1][j]+t[i][j-1];
+
+    }
+}
+
+
+
+
+        return t[m-1][n-1];
+        
+    }
+};
